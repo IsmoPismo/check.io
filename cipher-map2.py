a0ffaf89c8e8c28f6c6966450b5a38a82d81b48b
Ismar Sacirovic <ismarguitar@gmail.com>
2018-01-07 17:06:45 +0100

    Started solving cipher-map2

--- cipher-map2.py
+++ cipher-map2.py
@@ -1,14 +1,32 @@
 def recall_password(cipher_grille, ciphered_password):
-    s1, s2, s3, s4 = cipher_grille
-    print(s1.index('X'))
-    print(s2.index('X'))
-    print(s3.index('X'))
-    print(s4.index('X'))
-    return ""
+    encrypted = ''
+    index = 0
+    index2 = 0
+    for i, row in enumerate(cipher_grille):
+        if row.count('X') == 1:
+            index = row.index('X')
+            encrypted += ciphered_password[i][index]
+        if row.count('X') == 2:
+            index = row.index('X')
+            index2 = row[index:].index('X') + index #Ovdje si stao, treba skontat drugi index
+            encrypted += ciphered_password[i][index] + ciphered_password[i][index2]
+
+    return encrypted
+
 
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
+    print(recall_password((
+         'X...',
+         '..X.',
+         'X..X',
+         '....'),
+        ('itdf',
+         'gdce',
+         'aton',
+         'qrdi')))
+
     assert recall_password(
         ('X...',
          '..X.',
